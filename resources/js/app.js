3cf5d0ab948cde4b4e8060813a6ccf579dfe5860
Yiosh <iinoted@gmail.com>
2020-09-16 22:36:32 +0200

    setup history mode

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-2', require('./components/Example2.vue').default);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,15 +1,22 @@
 import VueRouter from "vue-router";
 import ExampleComponent from "./components/ExampleComponent";
+import Example2 from "./components/Example2";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: ExampleComponent
+  },
+  {
+    path: "/second",
+    name: "second",
+    component: Example2
   }
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes
 });
 
